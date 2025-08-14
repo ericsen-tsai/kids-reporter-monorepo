b46d4be25126bfb4d9ed3cb4687829a02ffc0ff3
nickhsine <nickhsine97753017@gmail.com>
2025-08-14 17:08:07 +0800

    feat(cms): add member list and adopt composite session stragedy
    - authenticate requests by `Authorization: Bearer <access_token>` header or keystone session cookie
    - provision member if needed

---
+++ packages/cms/lists/member.ts
@@ -0,0 +1,110 @@
+import { list, graphql } from '@keystone-6/core'
+import { text, timestamp, virtual } from '@keystone-6/core/fields'
+import { allowRoles, RoleEnum } from './utils/access-control-list'
+
+const operationAccessControl = allowRoles([
+  RoleEnum.FrontendHeadlessAccount,
+  RoleEnum.Admin,
+  RoleEnum.Owner,
+])
+
+const filterAccessControl = ({ session }: { session?: any }) => {
+  const userRole = session.data.role
+
+  if (userRole === RoleEnum.Admin || userRole === RoleEnum.Owner) {
+    return true
+  }
+
+  const memberID = session.data?.member?.id
+  if (memberID) {
+    return { id: { equals: memberID } }
+  }
+
+  return false
+}
+
+const listConfigurations = list({
+  fields: {
+    name: text({
+      label: '稱呼',
+    }),
+    email: text({
+      label: 'Email',
+      isIndexed: true,
+    }),
+    twreporter_user_id: text({
+      label: 'membership_user.users.id',
+      validation: { isRequired: true },
+      isIndexed: 'unique',
+    }),
+    role: text({
+      defaultValue: 'member',
+      ui: {
+        createView: {
+          fieldMode: 'hidden',
+        },
+        itemView: {
+          fieldMode: 'read',
+        },
+        listView: {
+          fieldMode: 'hidden',
+        },
+      },
+    }),
+    twoFactorAuth: virtual({
+      field: graphql.field({
+        type: graphql.JSON,
+        async resolve() {
+          return {
+            bypass: true,
+          }
+        },
+      }),
+      ui: {
+        createView: {
+          fieldMode: 'hidden',
+        },
+        itemView: {
+          fieldMode: 'read',
+        },
+        listView: {
+          fieldMode: 'hidden',
+        },
+      },
+    }),
+    createdAt: timestamp({
+      defaultValue: { kind: 'now' },
+    }),
+    updatedAt: timestamp({
+      db: {
+        updatedAt: true,
+      },
+    }),
+  },
+  ui: {
+    listView: {
+      initialColumns: ['id', 'name', 'email'],
+    },
+  },
+  db: {
+    idField: {
+      kind: 'cuid',
+    },
+  },
+  access: {
+    operation: {
+      query: operationAccessControl,
+      create: operationAccessControl,
+      update: operationAccessControl,
+      delete: operationAccessControl,
+    },
+    filter: {
+      query: filterAccessControl,
+      update: filterAccessControl,
+      delete: filterAccessControl,
+    },
+  },
+  hooks: {},
+})
+
+export default listConfigurations
