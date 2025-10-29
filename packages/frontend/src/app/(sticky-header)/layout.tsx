d8918b3587f96aa4f61fbf5af68fc0fb632d5135
Ericsen Tsai <bommer95175@gmail.com>
2025-10-29 16:29:01 +0800

    fix(frontend): adjust related posts layout and baodaozai callback event trigger

--- packages/frontend/src/app/(sticky-header)/layout.tsx
+++ packages/frontend/src/app/(sticky-header)/layout.tsx
@@ -22,7 +22,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           subscribeUrl={SUBSCRIBE_URL}
           donateUrl={DONATE_URL}
         />
-        <div className="flex grow">{children}</div>
+        <div className="flex w-full grow">{children}</div>
         <Baodaozai />
       </CallBaodaozaiProvider>
     </>
