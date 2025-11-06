2773b5ccafbcfe4fdbd8be3dd6ceb13c4532bb51
Ericsen Tsai <bommer95175@gmail.com>
2025-11-06 15:59:55 +0800

    feat(frontend): replace default avatar PNG with SVG and implement membership layout
    - Removed the old default avatar PNG and added a new SVG version.
    - Created a new layout component for the membership section, including a header and authentication setter.
    - Added a member page that utilizes the new layout and displays user information.
    - Introduced a side menu for membership navigation and constants for menu items and mock user data.

---
+++ packages/frontend/src/modules/membership/member/index.tsx
@@ -0,0 +1,51 @@
+'use client'
+import { useMediaQuery } from '@kids-reporter/routing-ui'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+import MembershipSideMenu from '../components/side-menu'
+import { MEMBERSHIP_MENU_ITEMS, MOCK_USER } from '../constants'
+
+function Member() {
+  const user = MOCK_USER
+
+  const isMobile = useMediaQuery('(max-width: 768px)')
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!isMobile) {
+      router.replace(MEMBERSHIP_MENU_ITEMS[0].href)
+    }
+  }, [isMobile, router])
+
+  return (
+    <div className="flex w-full flex-col items-center bg-neutral-100">
+      <div className="flex w-full flex-col items-center gap-8 pt-6 pb-40 tablet:items-start">
+        <div className="flex w-full flex-col items-center gap-4 tablet:hidden">
+          <div className="h-[136px] w-[136px] overflow-hidden rounded-full">
+            <Image
+              width={136}
+              height={136}
+              src={user.avatar}
+              alt={user.name}
+              className="h-full w-full bg-white object-cover"
+            />
+          </div>
+          <div className="flex flex-col items-center gap-0">
+            <h2 className="text-center prose-h5-small text-neutral-900">
+              {user.name}
+            </h2>
+            <p className="mt-0 text-center prose-p1 text-neutral-700">
+              {user.id}
+            </p>
+          </div>
+        </div>
+
+        <MembershipSideMenu />
+      </div>
+    </div>
+  )
+}
+
+export default Member
