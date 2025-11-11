117c8dfd7488a94761773abd025a5a8ec04e0986
Ericsen Tsai <bommer95175@gmail.com>
2025-11-11 11:47:36 +0800

    fix(frontend): enhance baodaozai visibility management
    - Added cleanup logic to reset the hide state when the BaodaozaiVisibilitySetter component unmounts.
    - Introduced innerHide state in the Baodaozai component to manage visibility during initialization.

--- packages/frontend/src/services/call-baodaozai/components/baodaozai-visibility-setter.tsx
+++ packages/frontend/src/services/call-baodaozai/components/baodaozai-visibility-setter.tsx
@@ -9,6 +9,11 @@ function BaodaozaiVisibilitySetter({ show }: { show: boolean }) {
   const { setHide } = baodaozaiProps
   useEffect(() => {
     setHide(!show)
+
+    return () => {
+      // reset hide state when component unmounts
+      setHide(false)
+    }
   }, [show, setHide])
   return null
 }
