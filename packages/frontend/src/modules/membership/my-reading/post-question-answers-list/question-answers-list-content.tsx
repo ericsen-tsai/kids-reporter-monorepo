bd15fa68cf9b37efd44502cc63252b76b69d52ce
Ericsen Tsai <bommer95175@gmail.com>
2025-11-17 10:22:48 +0800

    fix(frontend): adjust icon alignment in question-answers-list-content

--- packages/frontend/src/modules/membership/my-reading/post-question-answers-list/question-answers-list-content.tsx
+++ packages/frontend/src/modules/membership/my-reading/post-question-answers-list/question-answers-list-content.tsx
@@ -34,7 +34,7 @@ function QuestionAnswersListContent({
             key={answer.id}
             className="flex w-full flex-col gap-3 rounded-2xl bg-neutral-200 p-5"
           >
-            <div className="flex items-center gap-2">
+            <div className="flex items-start gap-2">
               <div className="mt-px flex h-6 w-6 shrink-0 items-center justify-center text-neutral-900">
                 <LightbulbIcon />
               </div>
