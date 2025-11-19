399cbb5bcba170a3a117f28c473d95f55eb4e151
Ericsen Tsai <bommer95175@gmail.com>
2025-11-19 14:34:11 +0800

    feat(frontend): add checkbox and switch components

---
+++ packages/frontend/src/components/switch.tsx
@@ -0,0 +1,28 @@
+import { cn } from '@kids-reporter/routing-ui'
+
+type SwitchProps = {
+  checked: boolean
+  onChange: (checked: boolean) => void
+}
+
+function Switch({ checked, onChange }: SwitchProps) {
+  return (
+    <button
+      onClick={() => onChange(!checked)}
+      className={cn(
+        'relative flex h-5 w-10 cursor-pointer items-center rounded-full p-0.5 transition-colors duration-100 ease-in-out',
+        checked ? 'bg-red-400' : 'bg-neutral-300'
+      )}
+      role="switch"
+      aria-checked={checked}
+    >
+      <span
+        className={`h-4 w-4 rounded-full bg-white shadow-[0px_0px_24px_0px_rgba(0,0,0,0.1)] transition-transform duration-200 ease-in-out ${
+          checked ? 'translate-x-5' : 'translate-x-0'
+        }`}
+      />
+    </button>
+  )
+}
+
+export default Switch
