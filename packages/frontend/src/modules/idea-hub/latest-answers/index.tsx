8c3e6606f99fc4805ef72b107a48ff5da9aa55fc
Ericsen Tsai <bommer95175@gmail.com>
2025-11-27 14:29:08 +0800

    fix(frontend): adjust padding

--- packages/frontend/src/modules/idea-hub/latest-answers/index.tsx
+++ packages/frontend/src/modules/idea-hub/latest-answers/index.tsx
@@ -29,7 +29,7 @@ function LatestAnswers() {
 
   return (
     <div className="mt-10 mb-14 flex w-[calc(100%+48px)] flex-col gap-8 tablet:mb-16 tablet:w-full desktop:mt-18 desktop:mb-24 hd:mt-24 hd:mb-30">
-      <div className="flex items-center gap-3 pl-8 tablet:pl-0">
+      <div className="flex items-center gap-3 pl-6 tablet:pl-0">
         <div className="h-8 w-1.5 rounded-md bg-yellow-400" />
         <h3 className="prose-h3-small font-swei text-neutral-900 desktop:prose-h3-large">
           最新回答
@@ -37,7 +37,7 @@ function LatestAnswers() {
       </div>
       <div
         className={cn(
-          'flex gap-6 overflow-x-auto px-8 tablet:grid tablet:grid-cols-3 tablet:overflow-x-hidden tablet:px-0',
+          'flex gap-6 overflow-x-auto px-6 tablet:grid tablet:grid-cols-3 tablet:overflow-x-hidden tablet:px-0',
           !isLoading && answers.length === 0 && 'tablet:grid-cols-1'
         )}
       >
