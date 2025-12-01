d9cdebeb278b4b45eb8023a3c9a023381cd2d0d7
Ericsen Tsai <bommer95175@gmail.com>
2025-12-01 14:50:29 +0800

    feat(frontend): adjust all answers UI padding

--- packages/frontend/src/modules/idea-hub/all-answers/index.tsx
+++ packages/frontend/src/modules/idea-hub/all-answers/index.tsx
@@ -8,14 +8,14 @@ function AllAnswers() {
   const isLoading = false
 
   return (
-    <div className="mt-10 mb-14 flex w-[calc(100%+48px)] flex-col gap-8 tablet:mb-16 tablet:w-[calc(100%+64px)] desktop:mt-18 desktop:mb-24 desktop:w-[calc(100%+96px)] hd:mt-24 hd:mb-30 hd:w-[calc(100%+112px)]">
-      <div className="flex items-center gap-3 pl-6 tablet:pl-8 desktop:pl-12 hd:pl-14">
+    <div className="mt-10 mb-14 flex w-[calc(100%+48px)] flex-col gap-8 tablet:mb-16 tablet:w-[calc(100%+64px)] desktop:mt-18 desktop:mb-24 desktop:w-[calc(100%+96px)] hd:mt-24 hd:mb-30 hd:w-screen">
+      <div className="flex items-center gap-3 pl-6 tablet:pl-8 desktop:pl-12 hd:pl-[calc(50vw-600px+64px)]">
         <div className="h-8 w-1.5 rounded-md bg-blue-400" />
         <h3 className="prose-h3-small font-swei text-neutral-900 desktop:prose-h3-large">
           所有回答
         </h3>
       </div>
-      <div className="flex scrollbar-thin snap-x snap-mandatory scroll-px-6 gap-6 overflow-x-auto px-6 tablet:scroll-px-8 tablet:px-8 desktop:scroll-px-12 desktop:px-12 hd:scroll-px-14 hd:px-14">
+      <div className="flex scrollbar-thin snap-x snap-mandatory scroll-px-6 gap-6 overflow-x-auto px-6 tablet:scroll-px-8 tablet:px-8 desktop:scroll-px-12 desktop:px-12 hd:scroll-pr-14 hd:scroll-pl-[calc(50vw-600px+64px)] hd:pr-14 hd:pl-[calc(50vw-600px+64px)]">
         {isLoading ? (
           <>
             {[1, 2, 3, 4, 5].map((index) => (
