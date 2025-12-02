8a03807b71f9b0e06a09cd2e7046cf03e9fd67b7
Ericsen Tsai <bommer95175@gmail.com>
2025-12-02 15:42:18 +0800

    fix(frontend): adjust heading font size for membership pages

--- packages/frontend/src/modules/membership/my-reading/index.tsx
+++ packages/frontend/src/modules/membership/my-reading/index.tsx
@@ -57,7 +57,7 @@ function MyReading() {
           <MembershipSideMenu />
         </div>
         <div className="flex flex-1 flex-col px-6 tablet:col-span-10 tablet:px-8 desktop:col-span-8 desktop:px-0">
-          <h1 className="mb-6 prose-h4-large font-swei text-neutral-900 desktop:mb-8">
+          <h1 className="mb-6 prose-h4-small font-swei text-neutral-900 desktop:mb-8 desktop:prose-h4-large">
             我的回答
           </h1>
           <PostQuestionAnswersList
