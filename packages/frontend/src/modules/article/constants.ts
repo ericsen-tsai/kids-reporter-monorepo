43c63caa32337488d3c0c44ed40151889a43a912
Ericsen Tsai <bommer95175@gmail.com>
2025-12-24 17:02:10 +0800

    feat(frontend): implement article toolbar with share functionality and responsive design
    - Added a new Toolbar component for article interactions, including share options for Facebook, Threads, Line, and a link copy feature.
    - Replaced the Sidebar and MobileSidebar with the new Toolbar in the Article component.
    - Introduced new constants for share icons and their respective click handlers.
    - Removed the deprecated constants file and updated the structure for better organization.

--- packages/frontend/src/modules/article/constants.ts
+++
@@ -1,3 +0,0 @@
-export const TABLET_OF_CONTENT_ANCHOR_PREFIX = 'toc-anchor'
-export const TABLET_OF_CONTENT_INDEX_PREFIX = 'toc-index'
-export const TABLET_OF_CONTENT_BACK_TO_TOP_KEY = 'back-to-top'
---
+++ packages/frontend/src/modules/article/constants.tsx
@@ -0,0 +1,60 @@
+import { toast } from 'sonner'
+
+import {
+  FaceBookIcon,
+  LineIcon,
+  LinkIcon,
+  ThreadsIcon,
+} from '@/icons/miscellaneous'
+
+export const TABLET_OF_CONTENT_ANCHOR_PREFIX = 'toc-anchor'
+export const TABLET_OF_CONTENT_INDEX_PREFIX = 'toc-index'
+export const TABLET_OF_CONTENT_BACK_TO_TOP_KEY = 'back-to-top'
+
+export const SHARE_ICONS = [
+  {
+    icon: <FaceBookIcon />,
+    label: 'Facebook',
+    onClick: () => {
+      const currentURL = window.location.href
+      const location =
+        'https://www.facebook.com/sharer/sharer.php?' +
+        `u=${encodeURIComponent(currentURL)}`
+      window.open(location, '_blank')
+    },
+  },
+  {
+    icon: <ThreadsIcon />,
+    label: 'Threads',
+    onClick: () => {
+      const currentURL = window.location.href
+      const location = `https://www.threads.net/intent/post?text=${encodeURIComponent(currentURL)}`
+      window.open(location, '_blank')
+    },
+  },
+  {
+    icon: <LineIcon />,
+    label: 'Line',
+    onClick: () => {
+      const currentURL = window.location.href
+      const location = `https://social-plugins.line.me/lineit/share?url=${encodeURIComponent(
+        currentURL
+      )}`
+      window.open(location, '_blank')
+    },
+  },
+  {
+    icon: (
+      <div className="flex aspect-square w-10 cursor-pointer appearance-none flex-col items-center justify-center rounded-full bg-neutral-600">
+        <LinkIcon />
+      </div>
+    ),
+    label: 'Link',
+    onClick: () => {
+      const currentURL = window.location.href
+      navigator.clipboard.writeText(currentURL).then(() => {
+        toast.success('已複製文章網址')
+      })
+    },
+  },
+]
