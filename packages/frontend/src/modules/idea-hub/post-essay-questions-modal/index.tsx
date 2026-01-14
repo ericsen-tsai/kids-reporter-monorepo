545af06d5c06b73f53f1fedcd972e66633d597ff
Ericsen Tsai <bommer95175@gmail.com>
2026-01-14 12:06:13 +0800

    feat(frontend): add IdeaHubShortcutIcon and update PostEssayQuestionsModal
    - Introduced IdeaHubShortcutIcon to the miscellaneous icons.
    - Updated PostEssayQuestionsModal to support a new 'in-article' mode, allowing for different icon rendering based on the mode.
    - Adjusted the toolbar component to pass the new mode prop.

--- packages/frontend/src/modules/idea-hub/post-essay-questions-modal/index.tsx
+++ packages/frontend/src/modules/idea-hub/post-essay-questions-modal/index.tsx
@@ -15,7 +15,11 @@ import {
   DialogTitle,
 } from '@/components/dialog'
 import { FALLBACK_IMG } from '@/constants'
-import { ArticleShortcutIconLarge, XIcon } from '@/icons/miscellaneous'
+import {
+  ArticleShortcutIconLarge,
+  IdeaHubShortcutIcon,
+  XIcon,
+} from '@/icons/miscellaneous'
 
 import ModalQuestionCard from './modal-question-card'
 
@@ -23,12 +27,14 @@ type PostEssayQuestionsModalProps = {
   postSlug: string
   open: boolean
   onClose: () => void
+  mode?: 'default' | 'in-article'
 }
 
 function PostEssayQuestionsModal({
   postSlug,
   open,
   onClose,
+  mode = 'default',
 }: PostEssayQuestionsModalProps) {
   const { data: post, isPending } = usePostEssayQuestionsByPostSlugQuery({
     slug: postSlug,
@@ -170,12 +176,21 @@ function PostEssayQuestionsModal({
                 </div>
 
                 <div className="flex items-center justify-center gap-4">
-                  <Link
-                    href={`/article/${postSlug}`}
-                    className="flex size-10 items-center justify-center gap-1 rounded-full bg-neutral-white/30 text-neutral-white hover:bg-neutral-white/50"
-                  >
-                    <ArticleShortcutIconLarge />
-                  </Link>
+                  {mode === 'default' ? (
+                    <Link
+                      href={`/article/${postSlug}`}
+                      className="flex size-10 items-center justify-center gap-1 rounded-full bg-neutral-white/30 text-neutral-white hover:bg-neutral-white/50"
+                    >
+                      <ArticleShortcutIconLarge />
+                    </Link>
+                  ) : (
+                    <Link
+                      href="/idea-hub"
+                      className="flex size-10 items-center justify-center gap-1 rounded-full bg-neutral-white/30 text-neutral-white hover:bg-neutral-white/50"
+                    >
+                      <IdeaHubShortcutIcon />
+                    </Link>
+                  )}
                   <DialogClose className="flex size-10 cursor-pointer items-center justify-center gap-1 rounded-full bg-neutral-white/30 text-neutral-white hover:bg-neutral-white/50">
                     <XIcon />
                     <span className="sr-only">Close</span>
