280ddec7835a5405e7cbd4d6767da41114946766
Ericsen Tsai <bommer95175@gmail.com>
2026-01-15 15:57:47 +0800

    feat(frontend): add metadata for membership and idea-hub pages

--- packages/frontend/src/app/(membership)/custom/page.tsx
+++ packages/frontend/src/app/(membership)/custom/page.tsx
@@ -1,4 +1,21 @@
+import { Metadata } from 'next'
+
+import { ContentType, KIDS_URL_ORIGIN } from '@/constants'
 import CustomModule from '@/modules/membership/custom'
+
+export const metadata: Metadata = {
+  title: '閱讀設定 - 少年報導者 The Reporter for Kids',
+  alternates: {
+    canonical: `${KIDS_URL_ORIGIN}/custom`,
+  },
+  openGraph: {
+    title: '閱讀設定 - 少年報導者 The Reporter for Kids',
+    description:
+      '《少年報導者》是由非營利媒體《報導者》針對兒少打造的深度新聞報導品牌，與兒童和少年一起理解世界，參與未來。在閱讀設定頁，你可以調整屬於自己偏好的閱讀模式。',
+    url: `${KIDS_URL_ORIGIN}/custom`,
+    type: ContentType.ARTICLE,
+  },
+}
 
 function CustomPage() {
   return <CustomModule />
--- packages/frontend/src/app/(membership)/myreading/page.tsx
+++ packages/frontend/src/app/(membership)/myreading/page.tsx
@@ -1,4 +1,21 @@
+import { Metadata } from 'next'
+
+import { ContentType, KIDS_URL_ORIGIN } from '@/constants'
 import MyReadingModule from '@/modules/membership/my-reading'
+
+export const metadata: Metadata = {
+  title: '我的回答 - 少年報導者 The Reporter for Kids',
+  alternates: {
+    canonical: `${KIDS_URL_ORIGIN}/myreading`,
+  },
+  openGraph: {
+    title: '我的回答 - 少年報導者 The Reporter for Kids',
+    description:
+      '《少年報導者》是由非營利媒體《報導者》針對兒少打造的深度新聞報導品牌，與兒童和少年一起理解世界，參與未來。在我的回答頁，你可以檢視和編輯你的選擇題、思辨題回答。',
+    url: `${KIDS_URL_ORIGIN}/myreading`,
+    type: ContentType.ARTICLE,
+  },
+}
 
 export default function MyReadingPage() {
   return <MyReadingModule />
