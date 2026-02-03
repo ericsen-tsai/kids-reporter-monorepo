4a2cdffbbc1332d9d62a6d1dae9acb8451ba1d5a
Ericsen Tsai <bommer95175@gmail.com>
2026-02-03 10:26:40 +0800

    fix(frontend): adjust news reading component style

--- packages/frontend/src/modules/article/components/news-reading.tsx
+++ packages/frontend/src/modules/article/components/news-reading.tsx
@@ -36,7 +36,7 @@ function NewsReading({ className, items }: NewsReadingProps) {
             key={option.name}
             onClick={() => setSelectedOption(option)}
             className={cn(
-              'w-[75px] cursor-pointer rounded-[30px] border-[2.5px] border-solid text-center prose-p2 transition-colors tablet:w-[90px]',
+              'w-[75px] cursor-pointer rounded-[30px] border-[2.5px] border-solid text-center prose-p2 transition-colors',
               isActive
                 ? 'border-blue-400 bg-blue-400 text-neutral-white hover:border-blue-500 hover:bg-blue-500 hover:text-neutral-white'
                 : 'border-blue-400 text-neutral-900 hover:border-blue-500 hover:bg-blue-500 hover:text-neutral-white'
@@ -54,30 +54,24 @@ function NewsReading({ className, items }: NewsReadingProps) {
   }
 
   return (
-    <div className="w-full px-4 tablet:px-[93px]">
+    <div className="w-full px-4">
       <div
         className={cn(
-          'mx-auto flex max-w-[584px] flex-col rounded-[20px] border border-neutral-200 p-6 tablet:flex-row tablet:items-stretch tablet:p-9 desktop:p-12 hd:max-w-[656px]',
+          'mx-auto mt-10 mb-10 flex max-w-[584px] flex-col rounded-[20px] border border-neutral-200 p-6 tablet:mt-20 tablet:mb-15 tablet:p-9 hd:max-w-[656px]',
           className
         )}
       >
-        <div className="flex flex-col items-center tablet:items-start">
-          <h3 className="mb-2 text-center prose-h6-small text-neutral-900 tablet:mb-4 tablet:prose-h6-large">
+        <div className="flex flex-col items-center">
+          <h3 className="mb-4 text-center prose-h6-small text-neutral-900 desktop:mb-6 desktop:prose-h6-large">
             讀報
           </h3>
-          <div className="mx-auto flex w-full flex-wrap justify-center gap-2.5 tablet:flex-col">
+          <div className="mx-auto flex w-full flex-wrap justify-center gap-2.5">
             {renderButtons}
           </div>
         </div>
 
-        <Divider className="my-4 tablet:hidden" />
-        <Divider
-          className="mx-6 hidden h-auto self-stretch tablet:block"
-          direction="vertical"
-        />
-        <div className="tablet:flex-1">
-          <div dangerouslySetInnerHTML={{ __html: selectedOption.code }} />
-        </div>
+        <Divider className="my-4" />
+        <div dangerouslySetInnerHTML={{ __html: selectedOption.code }} />
       </div>
     </div>
   )
