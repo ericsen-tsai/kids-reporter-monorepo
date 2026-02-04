7fd88a80e2418fafe55d469caeab98c1f3456a74
Ericsen Tsai <bommer95175@gmail.com>
2026-02-04 15:33:49 +0800

    feat(frontend): add background SVG and update TopicSlider component
    - Introduced a new background SVG for the home page.
    - Updated the TopicSlider component to use the new background image and adjusted the layout for better responsiveness.
    - Enhanced image handling for different screen sizes within the TopicSlider.

--- packages/frontend/src/modules/home/components/topic-slider/index.tsx
+++ packages/frontend/src/modules/home/components/topic-slider/index.tsx
@@ -24,87 +24,96 @@ function TopicSlider({ topics }: TopicSliderProp) {
   }
 
   return (
-    <div className="relative flex w-screen flex-col items-center justify-center bg-neutral-200 pt-6 pb-6 tablet:pb-20 desktop:pt-10 desktop:pb-24 hd:pt-12 hd:pb-30">
-      <div className="relative z-2 flex w-full flex-row items-center justify-center">
-        <Swiper
-          className="h-full !w-full"
-          onBeforeInit={(swiper) => {
-            swiperRef.current = swiper
-          }}
-          modules={[Navigation]}
-          loop
-          rewind
-          slidesPerView={1}
-          spaceBetween={20}
-          centeredSlides
-          breakpoints={{
-            768: {
-              slidesPerView: 'auto',
-              spaceBetween: 32,
-            },
-            1024: {
-              slidesPerView: 'auto',
-              spaceBetween: 48,
-            },
-            1440: {
-              slidesPerView: 'auto',
-              spaceBetween: 56,
-            },
-          }}
-        >
-          {topics.map((topic) => {
-            return (
-              <SwiperSlide key={topic.title} className="!h-auto tablet:!w-auto">
-                <TopicCard
-                  url={topic.url}
-                  image={topic.image}
-                  title={topic.title}
-                  subtitle={topic.subtitle}
-                />
-              </SwiperSlide>
-            )
-          })}
-        </Swiper>
-        <SwiperButton
-          variant="prev"
-          onClick={() => swiperRef.current?.slidePrev()}
-          className="absolute top-26 left-0 z-[900] -translate-y-1/2 scale-[0.625] tablet:top-42 tablet:left-[calc(50%-320px)] tablet:-translate-x-1/2 desktop:top-56 desktop:left-[calc(50%-416px)] desktop:scale-100 hd:top-64 hd:left-[calc(50%-544px)] hd:scale-100"
+    <div className="relative">
+      <div
+        role="presentation"
+        className='absolute -top-(--mobile-header-height) left-0 z-1 h-[calc(100%+var(--mobile-header-height))] w-full bg-[url("/assets/images/home/background.svg")] bg-cover bg-center desktop:-top-(--desktop-header-height) desktop:h-[calc(100%+var(--desktop-header-height))]'
+      ></div>
+      <div className="relative z-3 flex w-screen flex-col items-center justify-center pt-6 pb-6 tablet:pb-20 desktop:pt-10 desktop:pb-24 hd:pt-12 hd:pb-30">
+        <div className="relative z-2 flex w-full flex-row items-center justify-center">
+          <Swiper
+            className="h-full !w-full"
+            onBeforeInit={(swiper) => {
+              swiperRef.current = swiper
+            }}
+            modules={[Navigation]}
+            loop
+            rewind
+            slidesPerView={1}
+            spaceBetween={20}
+            centeredSlides
+            breakpoints={{
+              768: {
+                slidesPerView: 'auto',
+                spaceBetween: 32,
+              },
+              1024: {
+                slidesPerView: 'auto',
+                spaceBetween: 48,
+              },
+              1440: {
+                slidesPerView: 'auto',
+                spaceBetween: 56,
+              },
+            }}
+          >
+            {topics.map((topic) => {
+              return (
+                <SwiperSlide
+                  key={topic.title}
+                  className="!h-auto tablet:!w-auto"
+                >
+                  <TopicCard
+                    url={topic.url}
+                    image={topic.image}
+                    title={topic.title}
+                    subtitle={topic.subtitle}
+                  />
+                </SwiperSlide>
+              )
+            })}
+          </Swiper>
+          <SwiperButton
+            variant="prev"
+            onClick={() => swiperRef.current?.slidePrev()}
+            className="absolute top-26 left-0 z-[900] -translate-y-1/2 scale-[0.625] tablet:top-42 tablet:left-[calc(50%-320px)] tablet:-translate-x-1/2 desktop:top-56 desktop:left-[calc(50%-416px)] desktop:scale-100 hd:top-64 hd:left-[calc(50%-544px)] hd:scale-100"
+          />
+          <SwiperButton
+            variant="next"
+            onClick={() => swiperRef.current?.slideNext()}
+            className="absolute top-26 right-0 z-[900] -translate-y-1/2 scale-[0.625] tablet:top-42 tablet:right-[calc(50%-320px)] tablet:translate-x-1/2 desktop:top-56 desktop:right-[calc(50%-416px)] desktop:scale-100 hd:top-64 hd:right-[calc(50%-544px)] hd:scale-100"
+          />
+        </div>
+        <WaveIllustrations />
+        <Image
+          src="/assets/images/home/topic_baodaozai_illustration_s.svg"
+          alt="topic baodaozai illustration"
+          width={200}
+          height={100}
+          className="absolute right-4 bottom-10 z-4 tablet:hidden"
         />
-        <SwiperButton
-          variant="next"
-          onClick={() => swiperRef.current?.slideNext()}
-          className="absolute top-26 right-0 z-[900] -translate-y-1/2 scale-[0.625] tablet:top-42 tablet:right-[calc(50%-320px)] tablet:translate-x-1/2 desktop:top-56 desktop:right-[calc(50%-416px)] desktop:scale-100 hd:top-64 hd:right-[calc(50%-544px)] hd:scale-100"
+        <Image
+          src="/assets/images/home/topic_baodaozai_illustration_m.svg"
+          alt="topic baodaozai illustration"
+          width={256}
+          height={128}
+          className="absolute bottom-0 left-22 z-4 hidden tablet:block desktop:hidden"
+        />
+        <Image
+          src="/assets/images/home/topic_baodaozai_illustration_l.svg"
+          alt="topic baodaozai illustration"
+          width={336}
+          height={168}
+          className="absolute bottom-0 left-34 z-4 hidden desktop:block hd:hidden"
+        />
+        <Image
+          src="/assets/images/home/topic_baodaozai_illustration_l.svg"
+          alt="topic baodaozai illustration"
+          width={336}
+          height={168}
+          className="absolute bottom-0 left-[max(232px,10vw)] z-4 hidden hd:block"
         />
       </div>
-      <WaveIllustrations />
-      <Image
-        src="/assets/images/home/topic_baodaozai_illustration_s.svg"
-        alt="topic baodaozai illustration"
-        width={200}
-        height={100}
-        className="absolute right-4 bottom-10 z-4 tablet:hidden"
-      />
-      <Image
-        src="/assets/images/home/topic_baodaozai_illustration_m.svg"
-        alt="topic baodaozai illustration"
-        width={256}
-        height={128}
-        className="absolute bottom-0 left-22 z-4 hidden tablet:block desktop:hidden"
-      />
-      <Image
-        src="/assets/images/home/topic_baodaozai_illustration_l.svg"
-        alt="topic baodaozai illustration"
-        width={336}
-        height={168}
-        className="absolute bottom-0 left-34 z-4 hidden desktop:block hd:hidden"
-      />
-      <Image
-        src="/assets/images/home/topic_baodaozai_illustration_l.svg"
-        alt="topic baodaozai illustration"
-        width={336}
-        height={168}
-        className="absolute bottom-0 left-[max(232px,10vw)] z-4 hidden hd:block"
-      />
     </div>
   )
 }
