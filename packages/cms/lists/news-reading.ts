6d5d3272944e736195055b73067f89a74bd88129
Ericsen Tsai <bommer95175@gmail.com>
2026-02-05 11:38:54 +0800

    feat(cms): enforce dompurify in spotify embed code

--- packages/cms/lists/news-reading.ts
+++ packages/cms/lists/news-reading.ts
@@ -1,6 +1,7 @@
 import { list } from '@keystone-6/core'
 import { integer, relationship, text, timestamp } from '@keystone-6/core/fields'
 import type { ListConfig } from '@keystone-6/core/types'
+import DOMPurify from 'isomorphic-dompurify'
 
 import {
   allowAllRoles,
@@ -14,7 +15,7 @@ const NewsReadingGroupItem: ListConfig<any> = list({
       validation: { isRequired: true },
     }),
     embedCode: text({
-      label: 'Sopotify Iframe Embed Code',
+      label: 'Spotify Iframe Embed Code',
       validation: { isRequired: true },
       ui: { displayMode: 'textarea' },
     }),
@@ -68,6 +69,17 @@ const NewsReadingGroupItem: ListConfig<any> = list({
     label: 'News-reading-group-item',
     singular: 'News-reading-group-item',
     plural: 'News-reading-group-items',
+  },
+  hooks: {
+    resolveInput: ({ resolvedData, operation }) => {
+      if (
+        (operation === 'create' || operation === 'update') &&
+        resolvedData.embedCode != null
+      ) {
+        resolvedData.embedCode = DOMPurify.sanitize(resolvedData.embedCode)
+      }
+      return resolvedData
+    },
   },
   access: {
     operation: {
