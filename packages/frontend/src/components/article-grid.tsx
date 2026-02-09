9bd46298a89f38c4fba1a56ec3c9ed1d6a650b20
Ericsen Tsai <bommer95175@gmail.com>
2026-02-09 10:07:53 +0800

    fix(frontend): fix latest articles defects

--- packages/frontend/src/components/article-grid.tsx
+++ packages/frontend/src/components/article-grid.tsx
@@ -19,7 +19,7 @@ function ArticleGrid({
 
   return (
     <>
-      <div className="grid w-full grid-cols-1 gap-6 tablet:grid-cols-2">
+      <div className="grid w-full grid-cols-1 gap-6 tablet:grid-cols-2 tablet:gap-x-8 desktop:gap-x-12 hd:gap-x-14">
         {displayedArticles.map((article) => (
           <ArticleCard
             key={article.title}
