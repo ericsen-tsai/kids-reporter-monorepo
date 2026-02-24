5fe6b0e5b1eab64be68f7dda189b33f7a6f4d961
Ericsen Tsai <bommer95175@gmail.com>
2026-02-24 16:15:37 +0800

    feat(frontend): add new Baodaozai state types and binary files

---
+++ packages/frontend/src/services/call-baodaozai-test/types.ts
@@ -0,0 +1,15 @@
+export type GeneralBaodaozaiState =
+  | 'default'
+  | 'idel-sleep'
+  | 'idel-read'
+  | 'idel-enlighten'
+  | 'dialog-speaker'
+  | 'dialog-read'
+  | 'dialog-enlighten'
+
+export type QaBaodaozaiState =
+  | 'default'
+  | 'enlighten-send'
+  | 'enlighten-fault'
+  | 'enlighten-correct'
+  | 'enlighten-ask'
