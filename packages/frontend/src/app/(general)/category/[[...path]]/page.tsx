144510dc750596417eb36b3ba39a55753e5a0295
Ericsen Tsai <bommer95175@gmail.com>
2026-09-08 13:36:34 +0800

    refactor(frontend): enhance error handling for content API requests
    - Wrapped API request logic in try-catch blocks across multiple content API functions to handle 404 errors more gracefully.
    - Introduced `isContentApiNotFound` utility function to streamline error checking for not found responses.
    - Updated related functions in author, category, subcategory, and tag collections to return `undefined` for not found errors instead of throwing exceptions.
    - Improved logging for missing category posts in the category page component.

--- packages/frontend/src/app/(general)/category/[[...path]]/page.tsx
+++ packages/frontend/src/app/(general)/category/[[...path]]/page.tsx
@@ -225,8 +225,11 @@ export default async function Category({
   )
 
   if (!postsRes) {
-    emitStructured({ severity: 'WARNING', message: 'Empty related posts!' })
-    redirect(ERROR_PAGE)
+    emitStructured({
+      severity: 'WARNING',
+      message: `Category posts not found! ${path?.join('/') ?? ''}`,
+    })
+    notFound()
   }
 
   if ('subcategory' in postsRes && postsRes.subcategory) {
